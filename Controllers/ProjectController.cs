f4fa6118f03eb65a68e51b49c5236f8e9bc4f5c7
Zphinx.GZ <zeeyou.ou@gmail.com>
2026-02-12 23:48:42 +0700

    Final

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -5,59 +5,46 @@ namespace CSI402.Controllers;
 
 public class ProjectController : Controller
 {
-    public static List<ProjectUserViewModel> loggedInUsers = new List<ProjectUserViewModel>();
+    // list แยกกัน
+    public static List<ProjectUserViewModel> registerUsers = new();
+    public static List<ProjectUserViewModel> loginUsers = new();
 
     public IActionResult Index()
     {
-        ViewData["Title"] = "Project Page";
-        ViewData["Layout"] = "_ProjectLayout";
-
-        ViewBag.CartItemCount = 2;
-        ViewBag.LoggedInUsers = loggedInUsers;
-
         return View();
     }
 
-    public IActionResult Login()
-    {
-        ViewData["Title"] = "Login Page";
-        ViewData["Layout"] = "_ProjectLayout";
-
-        return View(new ProjectUserViewModel());
-    }
-
-    [HttpPost]
-    public IActionResult Login(ProjectUserViewModel model)
-    {
-        loggedInUsers.Add(new ProjectUserViewModel
-        {
-            Name = model.Name ?? "Unknown",
-            Email = model.Email,
-            Password = model.Password
-        });
-
-        return RedirectToAction("Index");
-    }
+    // ================= REGISTER =================
 
     public IActionResult Register()
     {
-        ViewData["Title"] = "Register Page";
-        ViewData["Layout"] = "_ProjectLayout";
-
+        ViewBag.Users = registerUsers;
         return View(new ProjectUserViewModel());
     }
 
     [HttpPost]
     public IActionResult Register(ProjectUserViewModel model)
     {
-        loggedInUsers.Add(new ProjectUserViewModel
-        {
-            Name = model.Name,
-            Lastname = model.Lastname,
-            Email = model.Email,
-            PhoneNumber = model.PhoneNumber
-        });
+        registerUsers.Add(model);
 
-        return RedirectToAction("Index");
+        ViewBag.Users = registerUsers;
+        return View(new ProjectUserViewModel());
+    }
+
+    // ================= LOGIN =================
+
+    public IActionResult Login()
+    {
+        ViewBag.Users = loginUsers;
+        return View(new ProjectUserViewModel());
+    }
+
+    [HttpPost]
+    public IActionResult Login(ProjectUserViewModel model)
+    {
+        loginUsers.Add(model);
+
+        ViewBag.Users = loginUsers;
+        return View(new ProjectUserViewModel());
     }
 }
